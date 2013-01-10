c122403dc395c4dfe2f599aaff763df2ff7df808
Seth Gibson <seth.gibson@intel.com>
2013-01-10 14:11:03 -0800

    adding comment and updating release config -sG

--- example-capture/src/testApp.cpp
+++ example-capture/src/testApp.cpp
@@ -3,10 +3,16 @@
 //--------------------------------------------------------------
 void testApp::setup()
 {
+	//call EnableRGB with false to set VGA mode, true to set WXGA mode
 	_pxcu.EnableRGB(false);
+
+	//call EnableDepth, EnableGesture, and EnableIR with false to capture raw data,
+	//call with true to capture raw data and load the data into ofTextures
 	_pxcu.EnableDepth(true);
 	_pxcu.EnableGesture(true);
 	_pxcu.EnableIR(true);
+
+	//Start the device and sdk session
 	_pxcu.Start();
 	ofSetWindowShape(1280,960);
 }
@@ -14,12 +20,14 @@ void testApp::setup()
 //--------------------------------------------------------------
 void testApp::update()
 {
+    //Update the device's data buffers and textures
 	_pxcu.Update();
 }
 
 //--------------------------------------------------------------
 void testApp::draw()
 {
+    //If texture capture was specified, each map is available as an ofTexture
 	_pxcu.RGBTex.draw(0,0,640,480);
 	_pxcu.LabelTex.draw(640,0,640,480);
 	_pxcu.DepthTex.draw(0,480,640,480);
